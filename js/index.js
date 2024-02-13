ea7baae7a86c5b2729fcefcbf59d6772ff8d37c4
Martin Flischman <hem0inn@icloud.com>
2024-02-13 17:07:13 +0200

    Code refactor
    refactor: all the code including the html, css and javascript refactored to be more modern and DRY
    
    refactor: amend the instagram profile name to redirect to the correct profile
    
    feat: changed the footer year and text
    
    refactor: changed the stylesheet name from style to styles

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
+const toggleNav = () => {
+    document.body.classList.toggle("nav-open");
+}
+
+const closeNav = () => {
+    document.body.classList.remove("nav-open");
+}
+
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav-link");
 
-navToggle.addEventListener("click", () => {
-    document.body.classList.toggle("nav-open");
-});
+navToggle.addEventListener("click", toggleNav);
 
 navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
+    link.addEventListener('click', closeNav);
 });
